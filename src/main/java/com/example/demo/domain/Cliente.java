0a956ce1c501175c4feae7f1b229af5c73886897
Alvaro <alvim.lopez@gmail.com>
2024-06-06 11:03:39 -0300

    Pedido, EstadoPagamento e Pagamento - Parte 1

--- src/main/java/com/example/demo/domain/Cliente.java
+++ src/main/java/com/example/demo/domain/Cliente.java
@@ -39,6 +39,8 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente(){}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
@@ -108,6 +110,19 @@ public class Cliente implements Serializable {
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+	
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+	public void setTipo(Integer tipo) {
+		this.tipo = tipo;
 	}
 
 	@Override
