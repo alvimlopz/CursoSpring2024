97c4288d553b2fecf863f6d38caafa7a2dd84a4d
Alvaro <alvim.lopez@gmail.com>
2024-07-09 09:48:31 -0300

    Subtotal e total do Pedido

--- src/main/java/com/example/demo/repositories/ProdutoRepository.java
+++ src/main/java/com/example/demo/repositories/ProdutoRepository.java
@@ -12,9 +12,12 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.domain.Categoria;
 import com.example.demo.domain.Produto;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	@Transactional
 	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
 	Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias , Pageable pageRequest);
 
